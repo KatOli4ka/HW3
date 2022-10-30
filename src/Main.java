40d44f3d1dddf55486b0958cda1379b6c1d9f73f
KatOli4ka <katoli4ka87@mail.ru>
2022-10-30 12:57:43 +0500

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 33;
+        if (age>=18){
+        System.out.println("Поздравляем с совершеннолетием!");    }
+
+        int age1=13;
+        if (age1<18){
+            System.out.println("Вы еще не достигли совершеннолетия. Подождите еще немного");
+
+    }
+
+
+        }
+}
